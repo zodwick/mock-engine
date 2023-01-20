cd610f37b7fd3455a1ff61e1b62d5c84e2388b9c
zodwick <115868723+zodwick@users.noreply.github.com>
2023-01-20 20:01:37 +0530

    quiz component

--- client/src/components/quiz.jsx
+++ client/src/components/quiz.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
+import Questiions from "./questiions";
 
 export default function Quiz() {
+  function onNext() {
+    console.log("h");
+  }
+
+  function onPrev() {
+    console.log("hh");
+  }
   return (
-    <div>quiz</div>
-  )
+    <div className="container">
+      <h1 className="tittle text-light">Mock Quiz</h1>
+
+      {/* display questions */}
+      <Questiions />
+      <div>
+        <button className="btn prev" onClick={onNext}>
+          previous
+        </button>
+        <button className="btn next" onClick={onPrev}>
+          next
+        </button>
+      </div>
+    </div>
+  );
 }
